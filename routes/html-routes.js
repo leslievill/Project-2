8b398e5d717b3d826af92b8a191b372e6109c2ed
kent28808 <kktanoue@gmail.com>
2020-10-28 20:42:49 -0700

    pseudocode route files

---
+++ routes/html-routes.js
@@ -0,0 +1,5 @@
+// require path that can use relative routes for the HTML.
+// require middleware for verifying in user is logged in.
+// create a route that sends user to members page if already logged in.
+// create a route that sends user to members page if they are already logged in and try to access the login page.
+// create a route that will direct a user who is not logged that tries to access the members page to the signup page.
---
+++ routes/post-routes.js
@@ -0,0 +1,5 @@
+// create a route for getting all the posts.
+// create a route for retrieving a single post.
+// create a route for saving a new post.
+// create a delete route for deleting of specific posts.
+// create a put route for updating posts.

b903ccc807990143e0829d4d73341bf28266c1e6
kent28808 <kktanoue@gmail.com>
2020-10-29 17:56:47 -0700

    HTML routes updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,45 @@
 // Require the models and passport from config directory.
-// Use passport to authenticate. 
+const db = require("../models");
+const passport = require("../config/passport");
+
+// Using passport to authenticate. 
+module.exports = function (app) {
+    app.post("/api/login", passport.authenticate("local"), (req, res) => {
+        res.json({
+            email: req.user.email,
+            id: req.user.id
+        });
+    });
+}
 // Create a route for creating a new user account.
+app.post("/api/signup", (req, res) => {
+    db.User.create({
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(() => {
+            res.redirect(307, "/api/login");
+        })
+        .catch(err => {
+            res.status(401).json(err);
+        });
+});
 // Create a route for user logout.
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
 // Create a route returning the user's data to be used on the client side.
+app.get("/api/user_data", (req, res) => {
+    if (!req.user) {
+        // The user is not logged in, send back an empty object
+        res.json({});
+    } else {
+        // Otherwise send back the user's email and id
+        // Sending back a password, even a hashed password, isn't a good idea
+        res.json({
+            email: req.user.email,
+            id: req.user.id
+        });
+    }
+});
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,33 @@
 // require path that can use relative routes for the HTML.
+const path = require("path");
+const db = require("../models");
 // require middleware for verifying in user is logged in.
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 // create a route that sends user to members page if already logged in.
-// create a route that sends user to members page if they are already logged in and try to access the login page.
-// create a route that will direct a user who is not logged that tries to access the members page to the signup page.
+module.exports = function (app) {
+    app.get("/", (req, res) => {
+        if (req.user) {
+            res.redirect("/members");
+        }
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
+    // create a route that sends user to members page if they are already logged in and try to access the login page.
+    app.get("/login", (req, res) => {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            res.redirect("/members");
+        }
+        res.sendFile(path.join(__dirname, "../public/login.html"));
+    });
+    app.get("/futureEndeavor", async(req, res) => {
+        res.sendFile(path.join(__dirname, "../public/futureEndeavor.html"));
+        db.Post.findAll({}).then(function(data) {
+          console.log('All Posts: ', data)
+          res.render("index", {data});
+        });
+      });
+    // create a route that will direct a user who is not logged that tries to access the members page to the signup page.
+    app.get("/members", isAuthenticated, (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/members.html"));
+    });
+};
